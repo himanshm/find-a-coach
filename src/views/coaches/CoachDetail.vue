f5e99e3d1f36d02cb087e6b2f806d8c36f5ebd12
himansh <sai.himanshum011@gmail.com>
2023-10-18 01:05:37 +0530

    Register routes Add Route page components - Add main layout and base styling Wire up pages with dummy content
    On branch main-layout
    Changes to be committed:
    	modified:   src/App.vue
    	new file:   src/components/layout/TheHeader.vue
    	modified:   src/main.js
    	modified:   src/router/index.js
    	new file:   src/views/NotFound.vue
    	new file:   src/views/coaches/CoachDetail.vue
    	new file:   src/views/coaches/CoachRegistration.vue
    	new file:   src/views/coaches/CoachesList.vue
    	new file:   src/views/requests/ContactCoach.vue
    	new file:   src/views/requests/RequestReceived.vue

---
+++ src/views/coaches/CoachDetail.vue
@@ -0,0 +1,7 @@
+<template>
+    DETAILS FOR COACH
+    <!-- render the child route of this component which is contact route -->
+    <router-view></router-view>
+    <!-- add a router-link to navigate to contact page through a dynamic id (hard-coded for now) -->
+    <router-link to="/coaches/c1/contact">Contact</router-link>
+</template>
